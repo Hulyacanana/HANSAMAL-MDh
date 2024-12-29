7a819811f621d81dd8fa79f190d278b5d171163d
Hulyacanana <158591864+Hulyacanana@users.noreply.github.com>
2024-12-29 18:07:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,5 @@ module.exports = {
 SESSION_ID: 'HANSAMAL-MD=rZVVmQ5J#qT8aP3ZDSJgXNXukRDZil63k8nZFN-uNHN9peBj_NRU',
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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' : process.env.GITHUB_AUTH_TOKEN,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'Hulyacanana' : process.env.GITHUB_USER_NAME,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '+94 70 427 6051' : process.env.OWNER_NUMBER
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94704276051' : process.env.OWNER_NUMBER
 };
